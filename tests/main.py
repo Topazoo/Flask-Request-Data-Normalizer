a43d716a8526997a316909126dfff7646a06b4c1
Peter Swanson <topazoo42@gmail.com>
2023-01-22 23:46:20 -0800

    [Tests] Add tests for base64 query string parsing with request body

--- tests/main.py
+++ tests/main.py
@@ -5,5 +5,6 @@ from test_basic_ops import *
 from test_query_params import *
 from test_query_params_with_body import *
 from test_b64_query_params import *
+from test_b64_query_params_with_body import *
 
 unittest.main(argv=sys.argv)
